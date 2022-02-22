cf075b15842d848141fce96e7093294a5df8c71d
leoguignard <leo.guignard@gmail.com>
2022-02-22 15:38:46 +0100

    test

--- test-dash.py
+++ test-dash.py
@@ -2,39 +2,42 @@ import dash
 import dash_html_components as html
 import plotly.graph_objects as go
 import dash_core_components as dcc
-import plotly.express as px
 from dash.dependencies import Input, Output
 
 
 app = dash.Dash()
 
-df = px.data.stocks()
-
+data_path = 'https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv'
+data = pd.read_csv(data_path)
 
 app.layout = html.Div(id = 'parent', children = [
-    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
-                                            'marginTop':40,'marginBottom':40}),
+    html.H1(id = 'H1', children = 'Covid-data',
+            style = {'textAlign':'center',
+                     'marginTop':40,'marginBottom':40}),
 
         dcc.Dropdown( id = 'dropdown',
         options = [
-            {'label':'Google', 'value':'GOOG' },
-            {'label': 'Apple', 'value':'AAPL'},
-            {'label': 'Amazon', 'value':'AMZN'},
+            {'label':'France', 'value':'France' },
+            {'label': 'United Kingdom', 'value':'United Kingdom'},
+            {'label': 'India', 'value':'India'},
             ],
-        value = 'GOOG'),
+        value = 'France'),
         dcc.Graph(id = 'bar_plot')
     ])
     
     
 @app.callback(Output(component_id='bar_plot', component_property= 'figure'),
               [Input(component_id='dropdown', component_property= 'value')])
+
+
 def graph_update(dropdown_value):
     print(dropdown_value)
-    fig = go.Figure([go.Scatter(x = df['date'], y = df['{}'.format(dropdown_value)],\
+    sub_data = df[df==dropdown_value]
+    fig = go.Figure([go.Scatter(x = df['date'], y = df['new_cases'],\
                      line = dict(color = 'firebrick', width = 4))
                      ])
     
-    fig.update_layout(title = 'Stock prices over time',
+    fig.update_layout(title = 'Number of new cases',
                       xaxis_title = 'Dates',
                       yaxis_title = 'Prices'
                       )
@@ -44,4 +47,3 @@ def graph_update(dropdown_value):
 
 if __name__ == '__main__': 
     app.run_server('0.0.0.0')
-    print('test')
