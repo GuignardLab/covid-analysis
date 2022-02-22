102de8115b0d9a7839ea70cd82bc47b91acbc08b
leoguignard <leo.guignard@gmail.com>
2022-02-22 15:39:35 +0100

    test

--- test-dash.py
+++ test-dash.py
@@ -3,6 +3,7 @@ import dash_html_components as html
 import plotly.graph_objects as go
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
+import pandas as pd
 
 
 app = dash.Dash()
