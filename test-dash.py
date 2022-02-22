1ec659f5c2a570b5ed4175e1cc834dbfd9fadef0
leoguignard <leo.guignard@gmail.com>
2022-02-22 16:08:04 +0100

    test

--- test-dash.py
+++ test-dash.py
@@ -8,87 +8,251 @@ import pandas as pd
 from scipy.ndimage.filters import generic_filter
 import numpy as np
 
-
-data_path = 'https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv'
-data = pd.read_csv(data_path)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
 app = dash.Dash()
 
-app.layout = html.Div(id = 'parent', children = [
-    html.H1(id = 'H1', children = 'Covid-data',
-            style = {'textAlign':'center',
-                     'marginTop':40,'marginBottom':40}),
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'cb5392c35661370d95f300086accea51/raw/'
+    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
+    'indicators.csv')
 
-        dcc.Dropdown( id = 'dropdown',
-        options = [
-            {'label':'France', 'value':'France' },
-            {'label': 'United Kingdom', 'value':'United Kingdom'},
-            {'label': 'India', 'value':'India'},
-            ],
-        value = 'France'),
-        dcc.Graph(id = 'bar_plot')
-    ])
-    
-    
-@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
-              [Input(component_id='dropdown', component_property= 'value')])
+available_indicators = df['Indicator Name'].unique()
 
+app.layout = html.Div([
+    html.Div([
 
-def graph_update(dropdown_value):
-    print(dropdown_value)
-    smoothing_size = 7
-    death_delay = 18
-    country_data = data[data['location']==dropdown_value]
-    cases = generic_filter(country_data.sort_values('date')['new_cases'],
-                           np.nanmean, size=smoothing_size)
-    death = generic_filter(country_data.sort_values('date')['new_deaths'],
-                           np.nanmean, size=smoothing_size)
-    if death_delay == 0:
-        lethality_rate = death/cases
-    else:
-        lethality_rate = death[death_delay:]/cases[:-death_delay]
-    lethality_rate[1<lethality_rate]=0
-    all_ticks = country_data['date'][len(country_data['date'])-len(lethality_rate):]
-    # ax.plot(all_ticks, lethality_rate, '-', label='Lethality ratio')
-    # ax.set_ylabel('Ratio death over #cases')
-    # ax.set_ylim(ylim)
-    # ax.legend()
-    # ax.set_title(country)
-    # if 1 < nb_plots:
-    #     for ax, val in zip(axes[1:], additional_plots):
-    #         Y = generic_filter(country_data.sort_values('date')[val],
-    #                            np.nanmean, size=smoothing_size)[to_keep]
-    #         ax.plot(Y, '-', label=val)
-    #         ax.set_ylabel(f'#{val}')
-    #         if 'per_hundred' in val:
-    #             ax.set_ylim(0, 100)
-    #         ax.legend()
-    # step_ticks = max(1, len(ticks)//6)
-    # ax.set_xticks(ticks[::step_ticks])
-    # ax.set_xticklabels(tick_labels[::step_ticks])
-    # ax.set_xlim(0, max(all_ticks))
-    # fig.tight_layout()
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Fertility rate, total (births per woman)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
 
-    fig = go.Figure([go.Scatter(x = all_ticks, y = lethality_rate,\
-                     line = dict(color = 'firebrick', width = 4))
-                     ])
-    
-    fig.update_layout(title = 'Lethality Rate',
-                      xaxis_title = 'Dates',
-                      yaxis_title = 'Ratio death over #cases'
-                      )
+        html.Div([
+            dcc.Dropdown(
+                id='crossfilter-yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='crossfilter-yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        'borderBottom': 'thin lightgrey solid',
+        'backgroundColor': 'rgb(250, 250, 250)',
+        'padding': '10px 5px'
+    }),
 
-    fig2 = go.Figure([go.Scatter(x = all_ticks, y = lethality_rate,\
-                      line = dict(color = 'firebrick', width = 4))
-                     ])
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Japan'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
 
-    fig2.update_layout(title = 'Lethality Rate',
-                      xaxis_title = 'Dates',
-                      yaxis_title = 'Ratio death over #cases'
-                      )
-    return (fig)
+    html.Div(dcc.Slider(
+        id='crossfilter-year--slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].max(),
+        step=None,
+        marks={str(year): str(year) for year in df['Year'].unique()}
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+])
 
+@app.callback(
+    dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
+    [dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-xaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value'),
+     dash.dependencies.Input('crossfilter-year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
 
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
+            customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
+            height=450,
+            hovermode='closest'
+        )
+    }
 
-if __name__ == '__main__': 
+def create_time_series(dff, axis_type, title):
+    return {
+        'data': [go.Scatter(
+            x=dff['Year'],
+            y=dff['Value'],
+            mode='lines+markers'
+        )],
+        'layout': {
+            'height': 225,
+            'margin': {'l': 20, 'b': 30, 'r': 10, 't': 10},
+            'annotations': [{
+                'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
+                'xref': 'paper', 'yref': 'paper', 'showarrow': False,
+                'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
+                'text': title
+            }],
+            'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
+            'xaxis': {'showgrid': False}
+        }
+    }
+
+@app.callback(
+    dash.dependencies.Output('x-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-xaxis-type', 'value')])
+def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df[df['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == xaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
+    return create_time_series(dff, axis_type, title)
+
+@app.callback(
+    dash.dependencies.Output('y-time-series', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
+     dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
+     dash.dependencies.Input('crossfilter-yaxis-type', 'value')])
+def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
+    dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Indicator Name'] == yaxis_column_name]
+    return create_time_series(dff, axis_type, yaxis_column_name)
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+if __name__ == '__main__':
     app.run_server('0.0.0.0')
+
+
+# data_path = 'https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv'
+# data = pd.read_csv(data_path)
+
+# app = dash.Dash()
+
+# app.layout = html.Div(id = 'parent', children = [
+#     html.H1(id = 'H1', children = 'Covid-data',
+#             style = {'textAlign':'center',
+#                      'marginTop':40,'marginBottom':40}),
+
+#         dcc.Dropdown( id = 'dropdown',
+#         options = [
+#             {'label':'France', 'value':'France' },
+#             {'label': 'United Kingdom', 'value':'United Kingdom'},
+#             {'label': 'India', 'value':'India'},
+#             ],
+#         value = 'France'),
+#         dcc.Graph(id = 'bar_plot')
+#     ])
+    
+    
+# @app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+#               [Input(component_id='dropdown', component_property= 'value')])
+
+
+# def graph_update(dropdown_value):
+#     print(dropdown_value)
+#     smoothing_size = 7
+#     death_delay = 18
+#     country_data = data[data['location']==dropdown_value]
+#     cases = generic_filter(country_data.sort_values('date')['new_cases'],
+#                            np.nanmean, size=smoothing_size)
+#     death = generic_filter(country_data.sort_values('date')['new_deaths'],
+#                            np.nanmean, size=smoothing_size)
+#     if death_delay == 0:
+#         lethality_rate = death/cases
+#     else:
+#         lethality_rate = death[death_delay:]/cases[:-death_delay]
+#     lethality_rate[1<lethality_rate]=0
+#     all_ticks = country_data['date'][len(country_data['date'])-len(lethality_rate):]
+#     # ax.plot(all_ticks, lethality_rate, '-', label='Lethality ratio')
+#     # ax.set_ylabel('Ratio death over #cases')
+#     # ax.set_ylim(ylim)
+#     # ax.legend()
+#     # ax.set_title(country)
+#     # if 1 < nb_plots:
+#     #     for ax, val in zip(axes[1:], additional_plots):
+#     #         Y = generic_filter(country_data.sort_values('date')[val],
+#     #                            np.nanmean, size=smoothing_size)[to_keep]
+#     #         ax.plot(Y, '-', label=val)
+#     #         ax.set_ylabel(f'#{val}')
+#     #         if 'per_hundred' in val:
+#     #             ax.set_ylim(0, 100)
+#     #         ax.legend()
+#     # step_ticks = max(1, len(ticks)//6)
+#     # ax.set_xticks(ticks[::step_ticks])
+#     # ax.set_xticklabels(tick_labels[::step_ticks])
+#     # ax.set_xlim(0, max(all_ticks))
+#     # fig.tight_layout()
+
+#     fig = go.Figure([go.Scatter(x = all_ticks, y = lethality_rate,\
+#                      line = dict(color = 'firebrick', width = 4))
+#                      ])
+    
+#     fig.update_layout(title = 'Lethality Rate',
+#                       xaxis_title = 'Dates',
+#                       yaxis_title = 'Ratio death over #cases'
+#                       )
+
+#     fig2 = go.Figure([go.Scatter(x = all_ticks, y = lethality_rate,\
+#                       line = dict(color = 'firebrick', width = 4))
+#                      ])
+
+#     fig2.update_layout(title = 'Lethality Rate',
+#                       xaxis_title = 'Dates',
+#                       yaxis_title = 'Ratio death over #cases'
+#                       )
+#     return (fig)
+
+
+
+# if __name__ == '__main__':
+#     app.run_server('0.0.0.0')
